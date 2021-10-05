87829ca0cae39a88a81681b5ea1a82e034f4f151
VisalPheng <visal.pheng@student.niptict.edu.kh>
2021-10-06 01:28:17 +0700

    Initial

---
+++ miniShop-Admin/src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyBYVOkDkknHolp5Jxv3wRz4Bxgel9WfwwQ',
+  authDomain: 'minishop-9d4b5.firebaseapp.com',
+  projectId: 'minishop-9d4b5',
+  storageBucket: 'minishop-9d4b5.appspot.com',
+  messagingSenderId: '425512278815',
+  appId: '1:425512278815:web:a479ba7b42a7f8b9b16e2a',
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+
+export default app
---
+++ miniShop-UI/src/pages/Success.jsx
@@ -0,0 +1,8 @@
+import { useLocation } from 'react-router'
+
+const Success = () => {
+  const location = useLocation()
+  return <div>Successful</div>
+}
+
+export default Success
